abd0aecb7fc30f495c087f0d133fcdb5820f8b96
Looser2199 <59087608+Looser2199@users.noreply.github.com>
2020-09-26 02:27:08 +0530

    Add files via upload

---
+++ SegTree.cpp
@@ -0,0 +1,54 @@
+#include<bits.stdc++.h>
+using namespace std;
+
+const int N=1e5+1;
+
+int n,T[N];
+
+void build(int a[]){
+
+	for(int i=0;i<n;i++)
+		T[i+n]=a[i];
+
+	for(int i=n-1;i>0;i--)
+		T[i]=T[i<<1]+T[i<<1|1];	
+}
+
+void modify(int p,int val){
+
+	p+=n;
+	T[p]=val;
+
+	for(int i=p;i>0;i>>=1)
+		T[i>>1]=T[i]+T[i^1];
+}
+
+int Sum(int l,int r ){
+
+	int sum=0;
+
+	l+=n,r+=n;
+
+	while(l<=r){
+
+		if(l&1)
+			ans+=T[l++];
+		
+		if(!(r&1))
+			ans+=T[r--];
+
+		l>>=1;
+		r>>=1;
+	}
+
+	return sum;
+}
+int main(){
+
+	cin>>n;
+
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	
+	build(a);
+}
